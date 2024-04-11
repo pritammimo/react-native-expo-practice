ec4ebda4351170891d17ca861e657182e6910efd
pritamcn <you@example.com>
2024-04-11 22:50:05 +0530

    main components

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image,ImageBackground, ScrollView, Button, Alert} from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View,Image,ImageBackground, ScrollView, Button, Pressable, Modal,StatusBar, ActivityIndicator, Alert} from 'react-native';
 const logoImg=require("./assets/adaptive-icon.png");
 export default function App() {
+  const [modalopen, setmodalopen] = useState(false);
   return (
     <View style={styles.container}>
+      <StatusBar  backgroundColor='lightgreen' barStyle='light-content' />
+      {/* <ActivityIndicator size="large" color="midnightblue" /> */}
       <ScrollView>
       <ImageBackground source={logoImg} style={styles.background}>
       <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
+      <Pressable onPress={()=>console.log("on Press")}>
       <View style={styles.firstview}>
          <Text style={styles.text}>First View</Text>
       </View>
+      <Button title='alert'
+      onPress={()=>Alert.alert("alert","alert message",[
+        {
+          text:"cancel",
+          onPress:()=>console.log("cancel")
+        },
+        {
+          text:"ok",
+          onPress:()=>console.log("ok")
+        }
+      ])}
+      color='midnightblue'
+      />
+      </Pressable>
+      <Pressable onPressIn={()=>console.log("in")} onPressOut={()=>console.log("out")}>
       <View style={styles.secondview}>
         <Text style={styles.text}>Second View</Text>
       </View>
+      </Pressable>
+      
       <Image source={logoImg} style={styles.logo}/>
       <Image source={{uri:"https://picsum.photos/200"}} style={styles.logo}/>
       <Button title='press' 
@@ -21,6 +43,20 @@ export default function App() {
       color='midnightblue'
       />
       </ImageBackground>
+      <Button title='open modal' onPress={()=>setmodalopen(!modalopen)}/>
+      <Modal visible={modalopen} 
+      animationType='slide'
+      presentationStyle='pageSheet'
+      onRequestClose={()=>setmodalopen(false)}
+      >
+      <View style={styles.secondview}>
+        <Text style={styles.text}>Modal View</Text>
+        <Button title='close'
+      color='midnightblue'
+      onPress={()=>setmodalopen(!modalopen)}
+      />
+      </View>
+      </Modal>
       </ScrollView>
     </View>
   );
