cef3f35944335c04a2b49cca812495952d954d69
pritamcn <you@example.com>
2024-04-12 22:53:28 +0530

    box

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Corecomponent from './src/module/CoreComponent'
-import Styles from './src/module/Styles'
+import Stylesmodule from './src/module/Styles'
 
 const App = () => {
   return (
     //<Corecomponent/>
-    <Styles/>
+    <Stylesmodule/>
   )
 }
 
--- src/module/Styles.jsx
+++ src/module/Styles.jsx
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const Styles = () => {
+export default function Stylesmodule () {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Styles</Text>
+      <View style={[styles.boxmodel,styles.secondview]}>
+        <Text>Red Box Model design</Text>
+      </View>
+      <View style={[styles.boxmodel,styles.firstview]}>
+        <Text>Green Box Model design</Text>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+    boxmodel:{
+        width:"100",
+        height:"100",
+        paddingHorizontal:10,
+        marginHorizontal:10,
+        marginHorizontal:20,
+        paddingVertical:20,
+        borderWidth:2,
+        borderColor:"purple",
+        borderRadius:10,
+    },
+    background:{
+     flex:1,
+    },
+    logo:{
+      width:200,
+      height:200,
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "plum",
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding:60
+    },
+    firstview:{
+      backgroundColor:"green",
+    },
+    secondview:{
+      backgroundColor:"red",
+    },
+    text:{
+      color:"white",
+    }
+  });
 
-export default Styles
