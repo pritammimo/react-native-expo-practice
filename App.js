2ce4d48d301a03605411caf4741802d013cf5908
pritamcn <you@example.com>
2024-04-12 22:25:59 +0530

    style module

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Corecomponent from './src/module/CoreComponent'
+import Styles from './src/module/Styles'
 
 const App = () => {
   return (
-    <Corecomponent/>
+    //<Corecomponent/>
+    <Styles/>
   )
 }
 
