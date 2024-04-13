507ccb16b43370efb7e7fbbbb4d55e06e211f8bc
pritamcn <you@example.com>
2024-04-14 01:12:43 +0530

    upgrade

--- src/module/DynamicuI.jsx
+++ src/module/DynamicuI.jsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View,Dimensions,useWindowDimensions} from 'react-native'
+
+import { StyleSheet, Text, View,Dimensions,useWindowDimensions, SafeAreaView,Platform} from 'react-native'
 
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
@@ -6,9 +7,16 @@ import { StatusBar } from 'expo-status-bar'
 export default function DynamicuI() {
     const Windowwidth = useWindowDimensions().width;
 const height = useWindowDimensions().height;
-console.log("height",height);
+console.log(Platform.OS)
   return (
-    <View style={styles.container}> 
+    <SafeAreaView style={styles.safecontainer}>
+        <StatusBar backgroundColor='lightgreen' barStyle='light-content'/>
+        <View>
+        <Text style={styles.text2}>Welcome</Text>
+        </View>
+        
+        
+    {/* <View style={styles.container}> 
     <StatusBar style='auto'/>
     <View style={[styles.box,{
         width:Windowwidth>500?"50%":"30%",
@@ -18,11 +26,34 @@ console.log("height",height);
     <Text style={{fontSize:Windowwidth>500?50:30}}>DynamicuI</Text>
     </View>
 
-    </View>
+    </View> */}
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+    text2: {
+        ...Platform.select({
+          ios: {
+            color: "purple",
+            fontSize: 24,
+            fontStyle: "italic",
+          },
+          android: {
+            color: "blue",
+            fontSize: 30,
+          },
+        }),
+        fontWeight: "bold",
+        textAlign: "center",
+      },
+    safecontainer:{
+        flex:1,
+        paddingTop:Platform.OS ==="android" ? 50:10,
+        backgroundColor:"plum",
+        alignItems: 'center',
+        //justifyContent: 'center',
+    },
     container: {
         flex:1,
         backgroundColor: "plum",
