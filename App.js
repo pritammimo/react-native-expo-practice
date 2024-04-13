6afc6201ab946d70423bafa20c5589bf7cd4b7fc
pritamcn <you@example.com>
2024-04-14 01:51:32 +0530

    upgrade

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Stylesmodule from './src/module/Styles'
 import DynamicuI from './src/module/DynamicuI'
+import List from './src/module/List'
 
 const App = () => {
   return (
     //<Corecomponent/>
     //<Stylesmodule/>
-    <DynamicuI/>
+    //<DynamicuI/>
+    <List/>
   )
 }
 
