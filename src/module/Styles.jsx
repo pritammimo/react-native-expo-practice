abfb7df69b29fa30a23fd1e6b4fbaa143f9527b9
pritamcn <you@example.com>
2024-04-13 14:50:14 +0530

    flex

--- src/module/Styles.jsx
+++ src/module/Styles.jsx
@@ -1,23 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
+import Box from '../components/box';
 
 export default function Stylesmodule () {
   return (
-    <View style={styles.container}>
-      <Text>Styles</Text>
-      <View style={[styles.boxmodel,styles.secondview]}>
+    <View style={styles.flexbox}>
+        <Box children="green box" style={{backgroundColor:"green",alignSelf:"flex-start"}}/>
+        <Box children="red box" style={{backgroundColor:"red",alignSelf:"flex-end"}}/>
+        <Box children="yellow box" style={{backgroundColor:"yellow",alignSelf:"center"}}/>
+        <Box children="orange box" style={{backgroundColor:"orange"}}/>
+        <Box children="blue box" style={{backgroundColor:"blue"}}/>
+       {/* <View style={styles.dark}>
+         <Text style={styles.darkText}>Styles Inheritance</Text>
+      </View>
+     
+      <View style={[styles.boxmodel,styles.secondview,styles.boxshadow]}>
         <Text>Red Box Model design</Text>
       </View>
-      <View style={[styles.boxmodel,styles.firstview]}>
+      <View style={[styles.boxmodel,styles.firstview,styles.androidshadow]}>
         <Text>Green Box Model design</Text>
-      </View>
+      // </View> */}
     </View>
   )
 }
 const styles = StyleSheet.create({
+  flexbox:{
+   borderColor:"red",
+   borderWidth:6,
+   flex:1,
+   marginTop:15,
+   //flexDirection:"row",
+   //justifyContent:"flex-end",
+   alignItems:"stretch"
+  },
     boxmodel:{
-        width:"100",
-        height:"100",
+        width:250,
+        height:250,
         paddingHorizontal:10,
         marginHorizontal:10,
         marginHorizontal:20,
@@ -46,8 +64,24 @@ const styles = StyleSheet.create({
     secondview:{
       backgroundColor:"red",
     },
-    text:{
-      color:"white",
+    dark:{
+      backgroundColor:"black",
+    },
+      darkText:{
+      color:"white", // Inheritance from view to text not work,but inheritance from text to text work
+    },
+    boxshadow:{
+      shadowColor:"#333333",
+      shadowOffset:{
+       width:6,
+       height:6
+      },
+      shadowOpacity:0.5,
+      shadowRadius:10,
+    },
+    androidshadow:{
+      elevation:10, // this propertty only works in android
+      shadowColor:"#333333",
     }
   });
 
